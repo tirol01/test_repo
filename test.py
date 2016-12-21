4b549706baad67019e96dd07796343367a31832f
y-matsuo <y-matsuo@sakura.ad.jp>
2016-12-22 01:47:39 +0900

    Add calc function

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 
+import sys
+
+def calc(a, b):
+  try:
+    result = float(a) + float(b)
+  except:
+    sys.stderr.write('input is not numeric.')
+    return None
+
+  if result.is_integer():
+    return int(result)
+  return result
+
+
 if __name__ == '__main__':
-  print('Hello World!')
+  ret = calc('hoge', 2) 
+  print(ret)
